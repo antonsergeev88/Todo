757ceb0725bf62a0768b23de0d6b5365e79bd1d7
Anton Sergeev <antonsergeev88@gmail.com>
2017-12-09 10:51:35 +0300

    Add TodoDetail story

---
+++ Todo/TodoDetailStory/TodoDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TodoDetailViewController.swift
+//  Todo
+//
+//  Created by Anton Sergeev on 09.12.2017.
+//  Copyright © 2017 Anton Sergeev. All rights reserved.
+//
+
+import UIKit
+
+class TodoDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
